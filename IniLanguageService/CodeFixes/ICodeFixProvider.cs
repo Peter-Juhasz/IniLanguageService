7ef9d1183678f79e0f5d697df28f14fa316bea66
Peter-Juhasz <peter.juhasz@live.com>
2015-06-11 09:23:06 +0200

    Introduced code fix provider.

---
+++ IniLanguageService/CodeFixes/ICodeFixProvider.cs
@@ -0,0 +1,13 @@
+﻿using IniLanguageService.CodeRefactorings;
+using Microsoft.VisualStudio.Text;
+using System.Collections.Generic;
+
+namespace IniLanguageService.CodeFixes
+{
+    public interface ICodeFixProvider
+    {
+        IEnumerable<string> FixableDiagnosticIds { get; }
+
+        IEnumerable<CodeAction> GetFixes(SnapshotSpan span);
+    }
+}
