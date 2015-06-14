7d602ddad184c5c2fb95ebdfbc17032d28790e5e
Peter-Juhasz <peter.juhasz@live.com>
2015-06-14 23:36:30 +0200

    Glyph in quick info.

--- IniLanguageService/QuickInfo/IniQuickInfoProvider.cs
+++ IniLanguageService/QuickInfo/IniQuickInfoProvider.cs
@@ -6,31 +6,45 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
 
-namespace IniLanguageService
+namespace IniLanguageService.QuickInfo
 {
     [Export(typeof(IQuickInfoSourceProvider))]
     [Name("INI Quick Info Provider")]
     [ContentType(IniContentTypeNames.Ini)]
     internal sealed class IniQuickInfoSourceProvider : IQuickInfoSourceProvider
     {
+#pragma warning disable 649
+
+        [Import]
+        private IGlyphService glyphService;
+
+#pragma warning restore 649
+
+
         public IQuickInfoSource TryCreateQuickInfoSource(ITextBuffer textBuffer)
         {
             return textBuffer.Properties.GetOrCreateSingletonProperty(
-                creator: () => new IniQuickInfoSource(textBuffer)
+                creator: () => new IniQuickInfoSource(textBuffer, glyphService)
             );
         }
 
 
         private sealed class IniQuickInfoSource : IQuickInfoSource
         {
-            public IniQuickInfoSource(ITextBuffer buffer)
+            public IniQuickInfoSource(ITextBuffer buffer, IGlyphService glyphService)
             {
                 _buffer = buffer;
+                _glyphService = glyphService;
             }
 
             private readonly ITextBuffer _buffer;
+            private readonly IGlyphService _glyphService;
 
+            private static readonly DataTemplate Template;
+        
 
             public void AugmentQuickInfoSession(IQuickInfoSession session, IList<object> quickInfoContent, out ITrackingSpan applicableToSpan)
             {
@@ -55,23 +69,34 @@ namespace IniLanguageService
                     if (!section.NameToken.IsMissing &&
                         section.NameToken.Span.Span.Contains(point))
                     {
-                        // construct content
-                        string content = $"(section) {sectionName}";
+                        // get glyph
+                        var glyph = _glyphService.GetGlyph(StandardGlyphGroup.GlyphGroupClass, StandardGlyphItem.GlyphItemPublic);
 
+                        // construct content
+                        var content = new QuickInfoContent
+                        {
+                            Glyph = glyph,
+                            Signature = sectionName
+                        };
+                        
                         var trivia = section.LeadingTrivia.SwitchToIfEmpty(section.TrailingTrivia);
                         if (section.LeadingTrivia.Any())
                         {
-                            content += Environment.NewLine +
-                                String.Join(Environment.NewLine,
-                                    trivia
-                                        .Select(t => t.Value)
-                                        .Select(v => v.Substring(1).Trim()) // TODO: move to value
-                                )
-                            ;
+                            content.Documentation = String.Join(Environment.NewLine,
+                                trivia
+                                    .Select(t => t.Value)
+                                    .Select(v => v.Substring(1).Trim()) // TODO: move to value
+                            );
                         }
-
+                        
                         // add to session
-                        quickInfoContent.Add(content);
+                        quickInfoContent.Add(
+                            new ContentPresenter
+                            {
+                                Content = content,
+                                ContentTemplate = Template,
+                            }
+                        );
                         applicableToSpan = snapshot.CreateTrackingSpan(section.NameToken.Span.Span, SpanTrackingMode.EdgeInclusive);
                         return;
                     }
@@ -84,23 +109,34 @@ namespace IniLanguageService
                     {
                         string propertyName = property.NameToken.Value;
 
-                        // construct content
-                        string content = $"(property) {propertyName} (in {sectionName})";
+                        // get glyph
+                        var glyph = _glyphService.GetGlyph(StandardGlyphGroup.GlyphGroupProperty, StandardGlyphItem.GlyphItemPublic);
 
+                        // construct content
+                        var content = new QuickInfoContent
+                        {
+                            Glyph = glyph,
+                            Signature = propertyName
+                        };
+                        
                         var trivia = property.LeadingTrivia.SwitchToIfEmpty(property.TrailingTrivia);
                         if (trivia.Any())
                         {
-                            content += Environment.NewLine +
-                                String.Join(Environment.NewLine,
-                                    trivia
-                                        .Select(t => t.Value)
-                                        .Select(v => v.Substring(1).Trim()) // TODO: move to value
-                                )
-                            ;
+                            content.Documentation = String.Join(Environment.NewLine,
+                                trivia
+                                    .Select(t => t.Value)
+                                    .Select(v => v.Substring(1).Trim()) // TODO: move to value
+                            );
                         }
 
                         // add to session
-                        quickInfoContent.Add(content);
+                        quickInfoContent.Add(
+                            new ContentPresenter
+                            {
+                                Content = content,
+                                ContentTemplate = Template,
+                            }
+                        );
                         applicableToSpan = snapshot.CreateTrackingSpan(property.NameToken.Span.Span, SpanTrackingMode.EdgeInclusive);
                         return;
                     }
@@ -109,6 +145,14 @@ namespace IniLanguageService
 
             void IDisposable.Dispose()
             { }
+
+
+            static IniQuickInfoSource()
+            {
+                var resources = new ResourceDictionary { Source = new Uri("pack://application:,,,/IniLanguageService;component/Themes/Generic.xaml", UriKind.RelativeOrAbsolute) };
+
+                Template = resources.Values.OfType<DataTemplate>().First();
+            }
         }
     }
 }
---
+++ IniLanguageService/QuickInfo/QuickInfoContent.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Media;
+
+namespace IniLanguageService.QuickInfo
+{
+    public class QuickInfoContent
+    {
+        public ImageSource Glyph { get; set; }
+
+        public string Signature { get; set; }
+
+        public string Documentation { get; set; }
+    }
+}
