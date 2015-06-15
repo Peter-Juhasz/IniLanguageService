7b30d58b57455b55f8f6a9c2d7631130ec153f3a
Peter-Juhasz <peter.juhasz@live.com>
2015-06-15 20:57:03 +0200

    Provide correct changed span for outlining tagger.

--- IniLanguageService/Outlining/IniOutliningTagger.cs
+++ IniLanguageService/Outlining/IniOutliningTagger.cs
@@ -17,13 +17,89 @@ namespace IniLanguageService
         public ITagger<T> CreateTagger<T>(ITextBuffer buffer) where T : ITag
         {
             return buffer.Properties.GetOrCreateSingletonProperty(
-                creator: () => new IniOutliningTagger()
+                creator: () => new IniOutliningTagger(buffer)
             ) as ITagger<T>;
         }
 
 
         private sealed class IniOutliningTagger : ITagger<IOutliningRegionTag>
         {
+            public IniOutliningTagger(ITextBuffer buffer)
+            {
+                _buffer = buffer;
+                _buffer.ChangedLowPriority += OnBufferChanged;
+            }
+
+            private readonly ITextBuffer _buffer;
+            
+            private void OnBufferChanged(object sender, TextContentChangedEventArgs e)
+            {
+                if (e.After != _buffer.CurrentSnapshot)
+                    return;
+
+                SnapshotSpan? changedSpan = null;
+
+                // examine old version
+                SyntaxTree oldSyntaxTree = e.Before.GetSyntaxTree();
+                IniDocumentSyntax oldRoot = oldSyntaxTree.Root as IniDocumentSyntax;
+
+                // find affected sections
+                IReadOnlyCollection<IniSectionSyntax> oldChangedSections = (
+                    from change in e.Changes
+                    from section in oldRoot.Sections
+                    where section.Span.IntersectsWith(change.OldSpan)
+                    orderby section.Span.Start
+                    select section
+                ).ToList();
+
+                if (oldChangedSections.Any())
+                {
+                    // compute changed span
+                    changedSpan = new SnapshotSpan(
+                        oldChangedSections.First().Span.Start,
+                        oldChangedSections.Last().Span.End
+                    );
+
+                    // translate to new version
+                    changedSpan = changedSpan.Value.TranslateTo(e.After, SpanTrackingMode.EdgeInclusive);
+                }
+
+                // examine current version
+                SyntaxTree syntaxTree = e.After.GetSyntaxTree();
+                IniDocumentSyntax root = syntaxTree.Root as IniDocumentSyntax;
+
+                // find affected sections
+                IReadOnlyCollection<IniSectionSyntax> changedSections = (
+                    from change in e.Changes
+                    from section in root.Sections
+                    where section.Span.IntersectsWith(change.NewSpan)
+                    orderby section.Span.Start
+                    select section
+                ).ToList();
+                
+                if (changedSections.Any())
+                {
+                    // compute changed span
+                    SnapshotSpan newChangedSpan = new SnapshotSpan(
+                        changedSections.First().Span.Start,
+                        changedSections.Last().Span.End
+                    );
+
+                    changedSpan = changedSpan == null
+                        ? newChangedSpan
+                        : new SnapshotSpan(
+                            changedSpan.Value.Start < newChangedSpan.Start ? changedSpan.Value.Start : newChangedSpan.Start,
+                            changedSpan.Value.End > newChangedSpan.End ? changedSpan.Value.End : newChangedSpan.End
+                        )
+                    ;
+                }
+
+                // notify if any change affects outlining
+                if (changedSpan != null)
+                    this.TagsChanged?.Invoke(this, new SnapshotSpanEventArgs(changedSpan.Value));
+            }
+            
+
             public IEnumerable<ITagSpan<IOutliningRegionTag>> GetTags(NormalizedSnapshotSpanCollection spans)
             {
                 ITextBuffer buffer = spans.First().Snapshot.TextBuffer;
